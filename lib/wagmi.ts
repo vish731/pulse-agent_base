20320b562f1709588639c66abd754246b2843e04
Gojo <vishalvc16vc@gmail.com>
2026-06-04 10:10:47 +0530

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,14 +1,10 @@
 "use client";
 
-import { http, createConfig, createStorage, cookieStorage } from "wagmi";
+import { http, createConfig } from "wagmi";
 import { base, baseSepolia } from "wagmi/chains";
-import { injected } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base, baseSepolia],
-  connectors: [injected()],
-  storage: createStorage({ storage: cookieStorage }),
-  ssr: true,
   transports: {
     [base.id]: http("https://mainnet.base.org"),
     [baseSepolia.id]: http("https://sepolia.base.org"),
@@ -20,4 +16,3 @@ declare module "wagmi" {
     config: typeof config;
   }
 }
-
